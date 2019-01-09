e3a7af0fd397e9c01a6fc3494df8677c382dc0f0
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-09 21:39:49 +0100

    Refactored

--- sv_modules.php
+++ sv_modules.php
@@ -9,38 +9,46 @@ namespace sv_100;
  * @since			1.0
  * @license			See license.txt or https://straightvisions.com
  */
-class sv_modules extends init{
-	static $scripts_loaded						= false;
 
-	public function __construct(){
+class sv_modules extends init {
+	public function __construct() {
 
 	}
-	public function init(){
-		$this->set_section_title('Modules');
-		$this->set_section_desc('Available Modules in SV 100 Theme.');
-		$this->set_section_type('settings');
-		
-		$this->set_module_title('Modules');
-		$this->set_module_desc('Available Modules in SV 100 Theme.');
 
-		add_action('admin_init', array($this, 'admin_init'));
-		if(!is_admin()){
-			add_action('init', array($this, 'load_settings'));
+	public function init() {
+		// Module Info
+		$this->set_module_title( 'SV Modules' );
+		$this->set_module_desc( __( 'This module manages all installed theme modules.', $this->get_module_name() ) );
+
+		// Section Info
+		$this->set_section_title( 'Modules' );
+		$this->set_section_desc( __( 'Available Modules in SV 100 Theme.', $this->get_module_name() ) );
+		$this->set_section_type( 'settings' );
+
+		// Loads Settings
+		$this->load_settings();
+
+		// Action Hooks
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+		if ( ! is_admin() ) {
+			add_action( 'init', array( $this, 'load_settings' ) );
 		}
 	}
-	public function admin_init(){
-		$this->get_root()->add_section($this);
-		//$this->load_settings();
-	}
-	public function load_settings(){
-		if(count($this->s) === 0) {
-			foreach ($this->get_modules_registered() as $module_name => $module_path) {
-				$s = static::$settings->create($this)
-				                      ->set_ID($module_name)
-				                      ->set_default_value(1)
-				                      ->load_type('checkbox');
 
-				$this->s[$module_name] = $s;
+	public function admin_init() {
+		$this->get_root()->add_section( $this );
+	}
+
+	public function load_settings() {
+		if ( count( $this->s ) === 0 ) {
+			foreach ( $this->get_modules_registered() as $module_name => $module_path ) {
+				$s = static::$settings->create( $this )
+					->set_ID( $module_name )
+					->set_default_value( 1 )
+					->load_type( 'checkbox' );
+
+				$this->s[ $module_name ] = $s;
 			}
 		}
 	}
