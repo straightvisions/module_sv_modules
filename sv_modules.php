4becdd0e797aa8d913134e47f61e7fc50ee6c7ce
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-11 16:39:17 +0100

    Added disable/enable modules setting

--- sv_modules.php
+++ sv_modules.php
@@ -24,29 +24,33 @@ class sv_modules extends init {
 		$this->set_section_title( 'Modules' );
 		$this->set_section_desc( __( 'Available Modules in SV 100 Theme.', $this->get_module_name() ) );
 		$this->set_section_type( 'settings' );
+		$this->get_root()->add_section( $this );
+
+		// Loads Scripts
+		static::$scripts->create( $this )
+		                ->set_source( $this->get_file_url( 'lib/js/backend.js' ), $this->get_file_path( 'lib/js/backend.js' ) )
+		                ->set_is_backend()
+		                ->set_type( 'js' );
 
 		// Loads Settings
 		$this->load_settings();
-
-		// Action Hooks
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
-
-		if ( ! is_admin() ) {
-			add_action( 'init', array( $this, 'load_settings' ) );
-		}
-	}
-
-	public function admin_init() {
-		$this->get_root()->add_section( $this );
 	}
 
 	public function load_settings() {
 		if ( count( $this->s ) === 0 ) {
+			$this->s['all_modules'] =
+				static::$settings->create( $this )
+                                 ->set_ID( 'all_modules' )
+                                 ->set_title( __( 'All Modules', $this->get_module_name() ) )
+                                 ->set_description( __( 'Enable or disable all modules.', $this->get_module_name() ) )
+                                 ->set_default_value( 1 )
+                                 ->load_type( 'checkbox' );
+
 			foreach ( $this->get_modules_registered() as $module_name => $module_path ) {
 				$s = static::$settings->create( $this )
-					->set_ID( $module_name )
-					->set_default_value( 1 )
-					->load_type( 'checkbox' );
+				                      ->set_ID( $module_name )
+				                      ->set_default_value( 1 )
+				                      ->load_type( 'checkbox' );
 
 				$this->s[ $module_name ] = $s;
 			}
