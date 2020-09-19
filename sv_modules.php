a452c4a6bf0a5d03f03e00b6e3fce192bf01fc06
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-19 18:42:20 +0200

    MIGRATE TO CORE V5.100 - REFACTORING

--- sv_modules.php
+++ sv_modules.php
@@ -1,32 +1,20 @@
 <?php
 	namespace sv100;
-	
-	/**
-	 * @version         4.008
-	 * @author			straightvisions GmbH
-	 * @package			sv100
-	 * @copyright		2019 straightvisions GmbH
-	 * @link			https://straightvisions.com
-	 * @since			1.000
-	 * @license			See license.txt or https://straightvisions.com
-	 */
-	
+
 	class sv_modules extends init {
 		public function init() {
 			$this->set_module_title( __( 'SV Modules', 'sv100' ) )
 				 ->set_module_desc( __( 'Manages all installed theme modules.', 'sv100' ) )
-				 ->load_settings()
-				 ->load_scripts()
-				 ->set_section_title( __( 'Modules', 'sv100' ) )
-			     ->set_section_desc( __( 'Available modules in the SV100 theme', 'sv100' ) )
-			     ->set_section_type( 'settings' )
-			     ->set_section_template_path( $this->get_path( 'lib/backend/tpl/settings.php' ) );
+				->set_section_title( $this->get_module_title() )
+				->set_section_desc( $this->get_module_desc() )
+				->set_section_type( 'settings' )
+				->set_section_template_path()
+				->set_section_order(5000);
 
 			if($this->get_is_expert_mode()) {
 				$this->get_root()->add_section( $this );
 			}
 		}
-	
 		protected function load_settings(): sv_modules {
 			$this->get_setting( 'all_modules' )
 				 ->set_title( __( 'All modules', 'sv100' ) )
@@ -39,19 +27,7 @@
 					 ->set_default_value( 1 )
 					 ->load_type( 'checkbox' );
 			}
-	
-			return $this;
-		}
-	
-		protected function load_scripts(): sv_modules {
-			// Register Styles
-			$this->get_script( 'default_js' )
-				 ->set_path( 'lib/backend/js/default.js' )
-				 ->set_is_backend()
-				 ->set_type( 'js' )
-				 ->set_deps( array(  'jquery' ) )
-				 ->set_is_enqueued();
-	
+
 			return $this;
 		}
 	}
